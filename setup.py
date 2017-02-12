cd2692c1c7ae8445b0110dd467a9caefaa3b3141
Yung-Chin Oei <yungchin@yungchin.nl>
2017-02-12 01:50:18 +0000

    add quick/dirty setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='objecthash',
+      version='0.1b',
+      description='Proposal for structured hashing of JSON objects',
+      url='https://github.com/benlaurie/objecthash',
+      modules=['objecthash'],
+     )
